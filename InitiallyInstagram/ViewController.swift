1b261442ea1641ffd889df1694e0b1e8d79058bf
ani6gup <ani6gup@gmail.com>
2016-02-26 22:45:06 -0700

    First Commit

---
+++ InitiallyInstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InitiallyInstagram
+//
+//  Created by Anisha Gupta on 2/25/16.
+//  Copyright © 2016 ani6gup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-InitiallyInstagram/Pods-InitiallyInstagram-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_InitiallyInstagramVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_InitiallyInstagramVersionString[];
+
